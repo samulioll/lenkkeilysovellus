c38577543454bcc2bcb3ea0284d6a15002f0404b
Samuli Ollikainen <sam.ollikainen@gmail.com>
2023-04-23 15:49:41 +0300

    restructured app folders

--- schema.sql
+++ schema.sql
@@ -44,24 +44,25 @@ CREATE TABLE comments (
     activity_id INTEGER REFERENCES activities,
     user_id INTEGER REFERENCES users,
     content TEXT,
-    date BOOLEAN,
-    visible TEXT
+    date TEXT,
+    seen BOOLEAN,
+    visible BOOLEAN
 );
 
-INSERT INTO routes (name, length, visible) VALUES ('PRESET 5km', 5, TRUE);
-INSERT INTO routes (name, length, visible) VALUES ('PRESET 7.5km', 7.5, TRUE);
-INSERT INTO routes (name, length, visible) VALUES ('PRESET 10km', 10, TRUE);
-INSERT INTO routes (name, length, visible) VALUES ('PRESET 10km', 10, TRUE);
-INSERT INTO routes (name, length, visible) VALUES ('PRESET 15km', 15, TRUE);
-INSERT INTO routes (name, length, visible) VALUES ('PRESET 20km', 20, TRUE);
-INSERT INTO routes (name, length, visible) VALUES ('PRESET 21.1km', 21.1, TRUE);
-INSERT INTO routes (name, length, visible) VALUES ('PRESET 30km', 30, TRUE);
-INSERT INTO routes (name, length, visible) VALUES ('PRESET 40km', 40, TRUE);
-INSERT INTO routes (name, length, visible) VALUES ('PRESET 40.2km', 40.2, TRUE);
-INSERT INTO routes (name, length, visible) VALUES ('PRESET 60km', 60, TRUE);
-INSERT INTO routes (name, length, visible) VALUES ('PRESET 80km', 80, TRUE);
+INSERT INTO routes (name, length, visible) VALUES ('PRESET | 5km', 5, TRUE);
+INSERT INTO routes (name, length, visible) VALUES ('PRESET | 7.5km', 7.5, TRUE);
+INSERT INTO routes (name, length, visible) VALUES ('PRESET | 10km', 10, TRUE);
+INSERT INTO routes (name, length, visible) VALUES ('PRESET | 15km', 15, TRUE);
+INSERT INTO routes (name, length, visible) VALUES ('PRESET | 20km', 20, TRUE);
+INSERT INTO routes (name, length, visible) VALUES ('PRESET | 21.1km', 21.1, TRUE);
+INSERT INTO routes (name, length, visible) VALUES ('PRESET | 30km', 30, TRUE);
+INSERT INTO routes (name, length, visible) VALUES ('PRESET | 40km', 40, TRUE);
+INSERT INTO routes (name, length, visible) VALUES ('PRESET | 40.2km', 40.2, TRUE);
+INSERT INTO routes (name, length, visible) VALUES ('PRESET | 60km', 60, TRUE);
+INSERT INTO routes (name, length, visible) VALUES ('PRESET | 80km', 80, TRUE);
 
 INSERT INTO groups (name, visible) VALUES ('TKO-Äly', TRUE);
+INSERT INTO groups (name, visible) VALUES ('Pohjoisen Parhaat', TRUE);
 
 INSERT INTO sports (name) VALUES ('walk');
 INSERT INTO sports (name) VALUES ('run');
